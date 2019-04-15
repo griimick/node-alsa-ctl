06e95fc95f21c32e03636208f77e01f3f862cdd7
Soumik Pradhan <soumik@antarx.tech>
2019-04-16 02:09:47 +0530

    Add support for get by numid or name

--- test/tests/test.js
+++ test/tests/test.js
@@ -11,5 +11,17 @@ async function test2 () {
 	let controls = await nac.getControls (0);
 	console.log (controls);
 }
+
+async function test3 () {
+	let controls = await nac.getControls (0, 27);
+	console.log (controls);
+}
+
+async function test4 () {
+	let controls = await nac.getControls (0, 'Master Playback Switch');
+	console.log (controls);
+}
 //test1();
-test2();
+//test2();
+//test3();
+//test4();
