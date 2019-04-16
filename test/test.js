8be2f202fbbcd7bb378b9eb9d09b101592508350
Soumik Pradhan <soumik@antarx.tech>
2019-04-16 14:42:30 +0530

    First draft for setControl

--- test/test.js
+++ test/test.js
@@ -18,10 +18,80 @@ async function test3 () {
 }
 
 async function test4 () {
-	let controls = await nac.getControls (0, 'Master Playback Switch');
+	let controls = await nac.getControls (0, 'Master Playback Volume');
+	console.log (controls);
+}
+
+async function test5 () {
+	query = { numid: '27',
+    iface: 'MIXER',
+    name: 'Master Playback Switch',
+    type: 'BOOLEAN',
+    access: 'rw------',
+    valuesCount: '1',
+    values: [ 'on' ] };
+	let controls = await nac.setControl (0, query);
+	console.log (controls);
+}
+async function test6 () {
+	query = { numid: '27',
+    iface: 'MIXER',
+    name: 'Master Playback Switch',
+    type: 'BOOLEAN',
+    access: 'rw------',
+    valuesCount: '1',
+    values: [ 'off' ] };
+	let controls = await nac.setControl (0, query);
+	console.log (controls);
+}
+
+async function test7 () {
+	query = { numid: '27',
+    iface: 'MIXER',
+    name: 'Master Playback Switch',
+    type: 'BOOLEAN',
+    access: 'rw------',
+    valuesCount: '1',
+    values: [ 'off' ] };
+	let controls = await nac.setControl (0, query);
+	console.log (controls);
+}
+
+async function test8 () {
+	query =	{ numid: '26',
+    iface: 'MIXER',
+    name: 'Master Playback Volume',
+    type: 'INTEGER',
+    access: 'rw---R--',
+    valuesCount: '1',
+    min: '0',
+    max: '127',
+    step: '0',
+    values: [ '127' ] };
+	let controls = await nac.setControl (0, query);
+	console.log (controls);
+}
+
+async function test9 () {
+	query =	{ numid: '26',
+    iface: 'MIXER',
+    name: 'Master Playback Volume',
+    type: 'INTEGER',
+    access: 'rw---R--',
+    valuesCount: '1',
+    min: '0',
+    max: '127',
+    step: '0',
+    values: [ '10' ] };
+	let controls = await nac.setControl (0, query);
 	console.log (controls);
 }
 //test1();
 //test2();
 //test3();
-//test4();
+test4();
+//test6();
+//test5();
+//test7();
+test8();
+test9();
